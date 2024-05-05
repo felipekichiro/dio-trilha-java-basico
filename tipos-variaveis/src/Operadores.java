93b99c887f68b7b35b091e99c422eadead2a1208
Felype Kichiro <felypekichiro@gmail.com>
2024-05-05 14:28:36 -0300

    exercicios do topico sintaxe java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+        int numero = 5;
+
+        //x repeticao
+        numero++;
+        //numero = numero +1;
+
+        System.out.println(numero);
+
+    }
+    
+}
